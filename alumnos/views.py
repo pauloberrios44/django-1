4bea3741b0711eb0eefc168e51a1d311b6cdff79
pauloberrios44 <166758722+pauloberrios44@users.noreply.github.com>
2024-06-07 18:23:40 -0400

    se agrega modelos

--- alumnos/views.py
+++ alumnos/views.py
@@ -1,7 +1,12 @@
 from django.shortcuts import render
+from .models import *
 
 # Create your views here.
 
 def index(request):
     context = {}
     return render(request, 'alumnos/index.html', context)
+
+def generos(request):
+    context = { "generos" : Genero.objects.all() }
+    return render(request, 'alumnos/generos.html', context)
